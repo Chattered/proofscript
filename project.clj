b6e2368e87dec02ae3e6621765067ab52aafbce4
Phil Scott <me@philscotted.com>
2013-01-23 19:45:23 +0000

    Add dependency on clojure_utils.

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.2"]
                  [the/parsatron "0.0.2-phlegmaticprogrammer"]
+                 [phlegmaticprogrammer/clojure_util "0.1.1"]
                  [slingshot "0.10.2"]
                  ]
   :dev-dependencies [[lein-autodoc "0.9.0"]]
   :test-path "src"
-  :aot []
+  :aot :all
   :plugins [[lein-swank "1.4.4"]])
