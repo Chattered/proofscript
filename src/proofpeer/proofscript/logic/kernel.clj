d9c56f244b99e0a11700451c96d3f327f47cbaf4
Phil Scott <philedscott@gmail.com>
2013-02-10 21:39:12 +0000

    Rename test file. Replace records with tagged vectors and fix context assignment in mk-type-constructor to just take the maximum of requested context and context of arguments.

--- src/proofpeer/proofscript/logic/kernel.clj
+++ src/proofpeer/proofscript/logic/kernel.clj
@@ -4,12 +4,15 @@
 (ns proofpeer.proofscript.logic.kernel
   "A kernel for the simply typed lambda calculus together with ZFC set
 theory. Formally: we allow terms to be rank-1 polymorphic and we allow
-type-constructors provided that all their type arguments havke kind *.
+type-constructors provided that all their type arguments have kind *.
 
 Additionally, all types are indexed by a context, which is used to associate
 axioms and definitions with types, terms and theorems. The context is an
 arbitrary value such that types with distinct context are taken to be
 distinct.
+
+All contexts are greater than the nil context and otherwise incomparable. See
+the documentation for mk-ty-constr.
 
 The nil context is reserved for the most logically secure parts of the
 kernel. The only type-constructors in the nil context are:
@@ -20,46 +23,46 @@ not permit any new axioms in the nil context.
 
 New axioms can be added into any other context according to whatever
 conventions are adopted by the context implementation. For instance, a context
-may wish to disallow overloaded constants and variadic type constructors.
-
-Functions with doc-strings introduced with CONTEXT are provided for clients
-providing implementations of contexts."
+may wish to disallow overloaded constants and variadic type constructors."
+  (:use phlegmaticprogrammer.clojure_util.core)
   )
 
-;;; Syntax
-(defrecord TypeConstr [cons context args])
-(defrecord TypeVar    [symbol])
-(defrecord Var        [symbol type])
-(defrecord Comb       [rator rand])
-(defrecord Abs        [bndV body])
-
-(defn- context-of-ty
+(defn context-of-ty
   [ty]
-  (condp = (class ty)
-      TypeConstr (.context ty)
-      TypeVar    nil))
+  (condp = (first ty)
+      :TypeConstr (let [[_ context _] (rest ty)]
+                    context)
+      :TypeVar    nil))
 
 (defn context-of-term
   "Returns the context of a given term."
   [term]
-  (condp = (type term)
-      Var   (context-of-ty   (.type  term))
-      Comb  (context-of-term (.rator term))
-      Abs   (context-of-term (.bndV  term))))
+  (condp = (first term)
+      :Var   (let [[_ ty] (rest term)] (context-of-ty ty))
+      :Comb  (let [[rator _] (rest term)] (context-of-term rator))
+      :Abs   (let [[bndV _] (rest term)] (context-of-term bndV))))
 
 (defn mk-ty-constr
-  "Apply a type constructor in a context. All type arguments must either belong
-  to the root context -- nil -- or belong to the given context.
-
-  Returns nil if the contexts do not match."
+  "Apply a type constructor in a context. The context of the returned type is
+the maximum of the given context and the contexts of all type argument
+contexts. Returns nil if no such maximum exists."
   [cons context & args]
-  (when (every? #(or (not %) (= (context-of-ty %) context)) args)
-    (->TypeConstr cons context args)))
+  (let [[maxContext valid]
+        (reduce #(let [[maxC valid] %1
+                       cx           (context-of-ty %2)] 
+                   (and valid
+                        (if (and maxC cx)
+                          (if (= maxC cx)
+                            [maxC true]
+                            [nil false])
+                          [(or maxC cx) true])))
+                [context true] args)]
+    (if valid [:TypeConstr cons maxContext args])))
 
 (defn mk-ty-var
   "Create a type variable."
   [sym]
-  (->TypeVar sym))
+  [:TypeVar sym])
 
 (defn mk-fun-ty
   "Apply the function type constructor in the root context."
@@ -70,64 +73,71 @@ providing implementations of contexts."
   "Returns the rator and rand type of a function type in a vector, or nil if
 the argument is not a function type."
   [ty]
-  (when (and (= (type ty) TypeConstr)
-             (= (.cons ty) '->))
-    (.args ty)))
+  (if-let [[tag cons context args] ty]
+    (when (and (= tag :TypeConstr)
+               (= cons '->))
+      (if (= (count args) 2)
+        args))))
   
 (defn type-of
   [tm]
-  (condp = (type tm)
-      Var   (.type tm)
-      Comb  (-> tm .rator type-of 1)
-      Abs   (mk-fun-ty
-             (-> tm .var type-of)
-             (-> tm .body type-of))))
+  (condp = (first tm)
+      :Var   (let [[_ ty] (rest tm)] ty)
+      :Comb  (let [[rator rand] (rest tm)] (second (dest-fun-ty (type-of rator))))
+      :Abs   (let [[v bod] (rest tm)]
+               (mk-fun-ty (type-of v) (type-of bod)))))
 
 (defn mk-var
   "Construct a typed variable."
   [sym type]
-  (->Var sym type))
+  [:Var sym type])
 
 (defn mk-comb
   "Construct a typed combination. Returns nil if the rator and rand types do not agree."
   [rator rand]
   (if-let [[dom-ty _] (dest-fun-ty (type-of rator))]
     (when (= dom-ty (type-of rand))
-      (->Comb rator rand))))
+      [:Comb rator rand])))
 
 (defn mk-abs
   "Construct a typed abstraction. Returns nil if the variable is not in the
 same context as the body."
   [bndV body]
-  (when (= (-> bndV .type .context) (context-of-term body))
-    (->Abs bndV body)))
+  (let [[tag sym ty] bndV]
+    (if (= tag :Var)
+      (when (= (context-of-ty ty) (context-of-term body))
+        [:abs bndV body])
+      (runtime-error "First argument to mk-abs must be a term variable."))))
 
 (defn mk-binop
   "Given a binary operator and two arguments, returns the term denoting the image of a operation applied to those arguments."
   [op x y]
-  (mk-comb (mk-comb op (mk-comb x)) y))
+  (mk-comb (mk-comb op x) y))
+
+(def alpha-ty
+  ^{:private true}
+  (mk-ty-var 'α))
 
 (def bool-ty
-  "The type of truth values."
-  (mk-ty-var 'bool))
+  (mk-ty-constr 'bool nil))
+
+(def eq-term
+  "Equality"
+  (mk-var '= (mk-fun-ty alpha-ty (mk-fun-ty alpha-ty bool-ty))))
 
 (def foo
-   (mk-fun-ty (mk-ty-var 1) (mk-ty-var 2)))
+  (mk-fun-ty alpha-ty bool-ty))
 
-;; (def eq
-;;   "Equality."
-;;   (let [eq-ty (mk-fun-ty (->TypeVar 3)
-;;                          (mk-fun-ty :bool (->TypeVar 'α)))]
-;;     (mk-var '= eq-ty)))
+(defn mk-eq
+  "Given x and y, returns the term x = y."
+  [x y]
+  (mk-binop eq-term x y))
 
-;; (defn mk-eq
-;;   "Given x and y, returns the term x = y."
-;;   [x y]
-;;   (mk-binop 
+(def a-ty
+  (mk-ty-constr     :At 1))
 
-(defrecord Theorem [assumptions concl])
+(def b-ty
+  (mk-ty-constr     :Bt 1))
 
-;; (defn refl [x]
-;;   "Given x, returns |- x = x"
-;;   [x]
-;;   (mk-
+(def f-ty
+  (mk-fun-ty        a-ty b-ty))
---
+++ src/proofpeer/proofscript/logic/tests/test-kernel.clj
@@ -0,0 +1,41 @@
+(ns proofpeer.proofscript.logic.tests.kernel
+  (:use clojure.test)
+  (:use proofpeer.proofscript.logic.kernel))
+
+(deftest term-tests
+  (let [contextA  :a
+        contextB  5
+        A-ty      (mk-ty-constr     :At contextA)
+        B-ty      (mk-ty-constr     :Bt contextA)
+        C-ty      (mk-ty-constr     :Ct contextA)
+        D-ty      (mk-ty-constr     :Dt contextB)
+        E-ty      (mk-ty-constr     :Et nil)
+        F-ty      (mk-ty-constr     :Ft nil)
+
+        f-ty      (mk-fun-ty        A-ty B-ty)
+        g-ty      (mk-fun-ty        f-ty C-ty)
+        h-ty      (mk-fun-ty        E-ty f-ty)
+
+        A         (mk-var           :A A-ty)
+        B         (mk-var           :B B-ty)
+        C         (mk-var           :C C-ty)
+        D         (mk-var           :D D-ty)
+        E         (mk-var           :E E-ty)
+        F         (mk-var           :F F-ty)
+
+        f         (mk-var           :f f-ty)
+        g         (mk-var           :g g-ty)
+        h         (mk-var           :h h-ty)
+
+        fA        (mk-comb          f A)]
+    (do
+      ; Context failure
+      (is (= nil (mk-ty-constr :foo nil A-ty D-ty)))
+      (is (= nil (mk-ty-constr :foo A-ty D-ty)))
+      (is (= nil (mk-fun-ty A-ty D-ty)))
+
+      ; Type failures
+      (is (= nil (mk-abs  E A)))
+      (is (= nil (mk-comb h B))))))
+
+      ; Constructions
